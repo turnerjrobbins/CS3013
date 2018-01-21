664e2a125d2bd870930c17ea785e384076b541ce
Turner Robbins <tjrobbins@wpi.edu>
2018-01-21 14:43:59 -0500

    added make targets for testing, changed mc0 to mc1

--- project1/v1/util.h
+++ project1/v1/util.h
@@ -4,28 +4,29 @@ void displayMenu() {
 	printf("1:\tlast\n");
 	printf("2:\tls\n");
 	printf("Option:");
-	/*
 	printf("a. add command : Adds a new command to the menu.");
 	printf("c. change directory : Changes process working directory");
 	printf("e. exit : Leave Mid-Day Commander");
-	printf("p. pwd : Prints working directory");	
-	*/
+	printf("p. pwd : Prints working directory");
 }
 
-void getCommand(char usrchar, char **file, char **arg) {
+static const int string_size = 128;
+static const in num_commands = 64;
 
+void getCommand(char usrchar, char **file, char **arg) {
+	static char **usrcommands = malloc(sizeof(char) * string_size * num_commands);
+	static int command_count = 0;
 	if(usrchar == '0') {
 		printf("----whoami----\n");
 		*file = "whoami";
 		arg[0] = "whoami";
 		arg[1] = NULL;
-	}else if (usrchar == '1') {
+	} else if (usrchar == '1') {
 		printf("----last----\n");
 		*file = "last";
 		arg[0] = "last";
 		arg[1] = NULL;
-	}
-	else if (usrchar == '2') {
+	} else if (usrchar == '2') {
 		printf("----list directory contents----\n");
 		*file = "ls";
 		arg[0] = "ls";
@@ -40,7 +41,16 @@ void getCommand(char usrchar, char **file, char **arg) {
 		arg[2] = strtok(arg[2], "\n");
 		arg[3] = NULL;
 
-	}else {
+	} else if (usrchar == 'a') {
+		printf("----Add Command----");
+
+	} else if (usrchar == 'c') {
+	
+	} else if (usrchar == 'e') {
+
+	} else if (usrchar = 'p') { 
+
+	} else {
 		printf("You picked incorrectly");
 	}
 
